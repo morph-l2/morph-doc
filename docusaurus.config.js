c6f1c5e0faf6fca9786806edf2aa86adf4c82994
Danny Fu <danny.fu@bitget.com>
2023-07-04 14:01:18 +0800

    fix: replace doc repo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/morphism-labs/morphism-doc',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/morphism-labs/morphism-doc',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
