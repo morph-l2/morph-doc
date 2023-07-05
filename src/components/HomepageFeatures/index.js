0395524e42bdaced7a817e62d68971f7b69ea5ff
Danny Fu <danny.fu@bitget.com>
2023-07-05 19:10:56 +0800

    fix: style change

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -53,6 +53,7 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
+        <h2 className={styles.homepageResource}>Resource</h2>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
