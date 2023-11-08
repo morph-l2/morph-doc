a981c82c569ef5680f7af92061c513c0a4eff22d
DannyFi <fuyinplus@gmail.com>
2023-11-08 12:06:41 +0800

    Theme (#11)
    * feature: add google-site-verification
    
    * fix: appId & apiKey
    
    * fix: banner
    
    * fix: banner
    
    * fix: change theme icon
    
    * fix: change img
    
    ---------
    
    Co-authored-by: Danny Fu <danny.fu@bitget.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import clsx from 'clsx';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import {useColorMode} from '@docusaurus/theme-common';
 import Layout from '@theme/Layout';
@@ -11,7 +10,7 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   const {colorMode, setColorMode} = useColorMode();
-  console.log('siteConfig', colorMode, siteConfig);
+
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <img src={`/img/index/banner_${colorMode}.svg`} alt="banner" />
