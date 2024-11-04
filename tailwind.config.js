102bfa7adef41c482888d1c770f58ba5f192ee97
Danny Fu <danny.fu@bitget.com>
2024-11-04 18:34:56 +0800

    fix: grid-cols

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
   corePlugins: {
     preflight: false, // disable Tailwind's reset
   },
+  safelist: [
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+  ],
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "../docs/**/*.mdx",
