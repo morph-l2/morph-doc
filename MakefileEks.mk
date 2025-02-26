33089392d365341460955e5ca7de931776d4c4a4
Danny Fu <danny.fu@bitget.com>
2025-02-26 17:50:20 +0800

    fix: add nginx

--- MakefileEks.mk
+++ MakefileEks.mk
@@ -9,7 +9,11 @@ build-bk-test-morph-test-qanet-to-morph-doc-qanet:
 	source ./makefile-function.sh && MFPnpmInstall && pnpm run build:qanet
 
 start-bk-test-morph-test-qanet-to-morph-doc-qanet:
-	mkdir -p logs/morph-doc && pnpm run deploy:qanet
+	# mkdir -p logs/morph-doc && pnpm run deploy:qanet
+	@echo "starting ngnix..."
+	nginx -g 'daemon off;'
+
+	
 
 healthcheck-bk-test-morph-test-qanet-to-morph-doc-qanet:
 	curl http://127.0.0.1:8080
