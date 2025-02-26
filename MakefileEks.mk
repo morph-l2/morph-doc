ebbb9d8c3f0152e474c5f6c75f030f02a6cb1748
Danny Fu <danny.fu@bitget.com>
2025-02-26 18:29:24 +0800

    fix: deploy

--- MakefileEks.mk
+++ MakefileEks.mk
@@ -7,6 +7,7 @@ LDFLAGS := -ldflags "$(LDFLAGSSTRING)"
 
 build-bk-test-morph-test-qanet-to-morph-doc-qanet:
 	source ./makefile-function.sh && MFPnpmInstall && pnpm run build:qanet
+	cp -rf nginx_sub_qanet.conf nginx_sub.conf
 
 start-bk-test-morph-test-qanet-to-morph-doc-qanet:
 	# mkdir -p logs/morph-doc && pnpm run deploy:qanet
@@ -21,9 +22,12 @@ healthcheck-bk-test-morph-test-qanet-to-morph-doc-qanet:
 
 build-bk-prod-morph-prod-mainnet-to-morph-doc:
 	source ./makefile-function.sh && MFPnpmInstall && pnpm run build:mainnet
+	cp -rf nginx_sub_mainnet.conf nginx_sub.conf
 
 start-bk-prod-morph-prod-mainnet-to-morph-doc:
-	mkdir -p logs/morph-doc && pnpm run deploy:mainnet
+	# mkdir -p logs/morph-doc && pnpm run deploy:mainnet
+	@echo "starting ngnix..."
+	nginx -g 'daemon off;'
 
 healthcheck-bk-prod-morph-prod-mainnet-to-morph-doc:
 	curl http://127.0.0.1:8080
